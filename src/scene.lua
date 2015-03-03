511ea00026a0f93d51b85823c8731a5e105c396b
Gavin Luo <luoxiangyu528@gmail.com>
2015-03-03 14:08:50 -0500

    first triangle drawn.

---
+++ src/scene.lua
@@ -0,0 +1,17 @@
+-- A polygonal approximation to a cylinder
+
+mat1 = gr.material({1.0, 0.0, 0.0}, {0.5, 0.7, 0.5}, 25)
+
+scene_root = gr.node('root')
+
+triangle = gr.mesh('triangle', { {0.0000, 0.0000, 0.0000},
+								 {0.0000, 1.0000, 0.0000},
+								 {1.0000, 0.0000, 0.0000}},
+							   { {0, 1, 2}})
+scene_root:add_child(triangle);
+triangle:set_material(mat1);
+
+white_light = gr.light({-100.0, 150.0, 400.0}, {0.9, 0.9, 0.9}, {1, 0, 0})
+orange_light = gr.light({400.0, 100.0, 150.0}, {0.7, 0.0, 0.7}, {1, 0, 0})
+
+gr.render(scene_root, 'triangle.png', 500, 500, {0, 0, 3}, {0, 0, -1}, {0, 1, 0}, 45, {0.3, 0.3, 0.3}, {white_light, orange_light}) 
