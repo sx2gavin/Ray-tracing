ee38c6cdbf98240dd78b1659227be67802a75afb
Gavin Luo <luoxiangyu528@gmail.com>
2015-03-05 10:59:47 -0500

    generated some good pictures.

--- data/sample.lua
+++ data/sample.lua
@@ -25,6 +25,7 @@ scene_root = gr.node('root')
 s1 = gr.sphere('s1')
 scene_root:add_child(s1)
 s1:set_material(mat1)
+s1:scale(0.5, 0.5, 0.5);
 
 s4 = gr.nh_sphere('s4', {1, 0, -1}, 1)
 scene_root:add_child(s4)
@@ -36,11 +37,11 @@ s2:set_material(mat2)
 s2:translate( 1, 0, 0)
 s2:scale(2, 0.5, 1)
 
--- s3 = gr.nh_box('s3', {0, -1, 1}, 0.25)
--- scene_root:add_child(s3)
--- s3:set_material(mat2)
+s3 = gr.nh_box('s3', {0, -1, 1}, 0.25)
+scene_root:add_child(s3)
+s3:set_material(mat2)
 
 white_light = gr.light({-6.0, 0.0, 6.0}, {0.9, 0.9, 0.9}, {1, 0, 0})
--- orange_light = gr.light({6.0, 0.0, 6.0}, {1.0, 0.0, 0.0}, {1, 0, 0})
+orange_light = gr.light({6.0, 0.0, 6.0}, {1.0, 0.0, 0.0}, {1, 0, 0})
 
-gr.render(scene_root, 'triangle.png', 500, 500, {0, 0, 7}, {0, 0, -1}, {0, 1, 0}, 50, {0.1, 0.1, 0.1}, {white_light}) 
+gr.render(scene_root, 'sample.png', 500, 500, {0, 0, 7}, {0, 0, -1}, {0, 1, 0}, 50, {0.1, 0.1, 0.1}, {orange_light, white_light}) 
